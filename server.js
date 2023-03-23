c2d1e002d44a9077e034acd3c793d0fe480905be
Mashotolucky <mashotolucky99@gmail.com>
2023-03-23 10:14:43 +0200

    add httpsa

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-var server = require( 'http' ).Server( app );
+var server = require( 'https' );
 
 const corsOptions = {
     origin: '*'
