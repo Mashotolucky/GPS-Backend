6b0be94dc42cbaebd1e2787aa21987a6530a4380
Mashotolucky <mashotolucky99@gmail.com>
2023-03-23 10:16:58 +0200

    ssl

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -10,7 +10,7 @@ const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPAS
 
 const pool = new Pool({
   connectionString: isProduction? process.env.DATABASE_URL : connectionString,
-  ssl: isProduction ? { rejectUnauthorized: false }: false,
+  // ssl: isProduction ? { rejectUnauthorized: false }: false,
 });
 
 module.exports = {
