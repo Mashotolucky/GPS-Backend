4c68f18b2b1e6ac35c9a16327ad5e328736e281b
Mashotolucky <mashotolucky99@gmail.com>
2023-03-23 10:21:19 +0200

    server app

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-var server = require( 'https' );
+var server = require( 'http' ).Server( app );
 
 const corsOptions = {
     origin: '*'
