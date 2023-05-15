2564d2ceb939e064a834da6a5a1a7618f09b5445
Mashotolucky <mashotolucky99@gmail.com>
2023-05-15 23:48:25 +0200

    database conn

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -5,7 +5,7 @@ const { Pool } = require("pg");
 const isProduction = process.env.NODE_ENV === "production";
 const database = process.env.NODE_ENV === "dev" ? process.env.PGDATABASE_TEST : process.env.PGDATABASE;
 
-const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${database}`;
+const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`;
 //const connectionString = `postgresql://root:12345@localhost:5432/esign`;
 
 const pool = new Pool({
