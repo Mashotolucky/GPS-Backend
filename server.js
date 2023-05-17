0bc0edf275b6ba9778b4e836f88bf6ae1e56115d
Mashotolucky <mashotolucky99@gmail.com>
2023-05-17 20:37:20 +0200

    http to https

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-var server = require( 'http' ).Server( app );
+var server = require( 'https' ).Server( app );
 
 const corsOptions = {
     origin: '*'
